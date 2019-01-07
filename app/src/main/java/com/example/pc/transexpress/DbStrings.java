c0129ca41a03daf07143cdc3f0b65a47ab88ad3b
koucham fadoua <fadouakoucham3@gmail.com>
2019-01-07 03:35:09 +0100

    commit

---
+++ app/src/main/java/com/example/pc/transexpress/DbStrings.java
@@ -0,0 +1,7 @@
+package com.example.pc.transexpress;
+public class DbStrings {
+    static final String DATABASE_URL="localhost:3306";
+    static final String DATABASE_NAME="mhs_example";
+    static final String USERNAME="root";
+    static final String PASSWORD ="12345678";
+}
